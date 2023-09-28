e8072596e8caf808a59e4cdf615685de97898ffb
Simran Mandal <simranmandal0211@gmail.com>
2023-09-28 16:10:06 +0530

    update and delete image from album is implemented

--- src/components/Imageslist/Imageslist.js
+++ src/components/Imageslist/Imageslist.js
@@ -58,6 +58,23 @@ export default function Imageslist( {albumId, onBackClick} ){
         }
     }
 
+    const handleEditImage = (image) => {
+        setTitle(image.title);
+        setImageUrl(image.imageUrl);
+        setEditImageId(image.id);
+        setShowForm(true);
+    }
+
+    const handleDeleteImage = (imageId) => {
+        try{
+            const updatedImages = images.filter((image) => image.id !== imageId);
+            setImages(updatedImages);
+            toast.success('Image deleted successfully!');
+        }catch(err){
+            console.error('Error deleting image: ', err);
+        }
+    }
+
     return (
         <>
             <div className="img-page">
@@ -108,32 +125,37 @@ export default function Imageslist( {albumId, onBackClick} ){
 
 
                 <div className="images-list-box">
-                    <div className="images-list">
-                        <div className="img-card">
-                            <div className="edit-delete-icon">
-                                <div>
-                                    <img alt="edit"
-                                        className="edit"
-                                        src={editIcon}
-                                    />
+                    {images.map((image, index) =>  (
+                        <div className="images-list">
+                            <div className="img-card"
+                                key={image.id}    
+                            >
+                                <div className="edit-delete-icon">
+                                    <div>
+                                        <img alt="edit"
+                                            className="edit"
+                                            src={editIcon}
+                                            onClick={() => handleEditImage(image)}
+                                        />
+                                    </div>
+                                            
+                                    <div>
+                                        <img alt="delete"
+                                            className="delete"
+                                            src={deleteIcon}
+                                            onClick={() => handleDeleteImage(image.id)}
+                                        />
+                                    </div>
                                 </div>
-                                        
-                                <div>
-                                    <img alt="delete"
-                                        className="delete"
-                                        src={deleteIcon}
-                                    />
-                                </div>
+                                
+                                <img src={image.imageUrl}
+                                    alt=""
+                                    className="url-image"
+                                />
+                                <h1>{image.title}</h1>
                             </div>
-                            {/* {image.imageUrl} */}
-                            
-                            <img src='https://tse3.mm.bing.net/th?id=OIP.6lUKCuJkx2nu5OaAe9OPRwHaF7&pid=Api&P=0&h=180'
-                                alt=""
-                                className="url-image"
-                            />
-                            <h1>Image title</h1>
                         </div>
-                    </div>
+                    ))}
 
                 </div>
 
